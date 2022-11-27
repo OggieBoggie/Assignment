5815791d41f7887eebbe421b65199d58aeb0f21b
OggieBoggie <tommy.nguyen.vanc@gmail.com>
2022-11-27 12:50:14 -0800

    Dark Theme and light theme function added

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const cthemeButton = document.querySelector(".themebutton")
+
+function changeTextLight () {
+        cthemeButton.textContent = "Light Theme"
+}
+
+function changeTextDark () {
+        cthemeButton.textContent = "Dark Theme"
+}
+
+// cthemeButton.addEventListener("click", changeText)
+
+function enableDarkTheme () {
+        document.body.classList.add("darktheme");
+}
+
+function disableDarkTheme () {
+        document.body.classList.remove("darktheme");
+}
+
+cthemeButton.addEventListener("click", () => {
+    if (cthemeButton.textContent != "Light Theme") {
+        changeTextLight()
+        enableDarkTheme()
+    }
+    else {
+        changeTextDark()
+        disableDarkTheme()
+    }
+
+})
